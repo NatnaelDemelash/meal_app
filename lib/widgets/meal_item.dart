b4002b0e96baafdb76eb521a7023feab36b93e31
Natnael Demelash <natidemelash074@gmail.com>
2023-06-08 15:51:51 +0300

    meal card style added

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,31 +10,36 @@ class MealItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-        children: [
-          FadeInImage(placeholder: MemoryImage(kTransparentImage), image: NetworkImage(meal.imageUrl),),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 40,
-            child: InkWell(
-              onTap: (){},
-              child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
-              color: Colors.black54,
-              child: Column(
-                children: [
-                  Text(meal.title, textAlign: TextAlign.center, maxLines: 2,
-                  softWrap: true, style: const TextStyle(color: Colors.white, fontSize: 20),),
-                  const SizedBox(height: 12),
-                  Row(
-                    children: [],
-                  )
-                ],
-              ),
-                      ),
-            ))
-        ],
+    return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        onTap: (){},
+        child: Stack(
+            children: [
+              FadeInImage(placeholder: MemoryImage(kTransparentImage), image: NetworkImage(meal.imageUrl),),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 40,
+                child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+                color: Colors.black54,
+                child: Column(
+                  children: [
+                    Text(meal.title, textAlign: TextAlign.center, maxLines: 2,
+                    softWrap: true, style: const TextStyle(color: Colors.white, fontSize: 20),),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: const [],
+                    )
+                  ],
+                ),
+                ),)
+            ],
+        ),
+      ),
     );
   }
 }
